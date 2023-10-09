05bdacda89af29e1b55fe658f2eb0c9b102f7e74
Kamil Berenhard <kamilberenhard@wp.pl>
2023-10-09 10:36:07 +0200

    project init

---
+++ src/main/java/pl/kamilberenhard/transped/TranSpedApplication.java
@@ -0,0 +1,13 @@
+package pl.kamilberenhard.transped;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranSpedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranSpedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kamilberenhard/transped/TranSpedApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kamilberenhard.transped;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranSpedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
