beb5fa16048d2f28032cd72d1c142c7b4fd0b8d3
Kamil Berenhard <kamilberenhard@wp.pl>
2023-10-10 10:47:46 +0200

    created Client model

---
+++ src/main/java/pl/kamilberenhard/transped/model/Client.java
@@ -0,0 +1,61 @@
+package pl.kamilberenhard.transped.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    private String street;
+
+    private String city;
+
+    private String postalCode;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
